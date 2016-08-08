46eb3ba385e55dda936abeb1d88742ea2d860d07
Josh Kloster <joshua.kloster@wsu.edu>
2016-08-07 20:46:45 -0700

    Added update script and moved plugins to seperate file

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=~/dotfiles
+FILES="vimrc vimrc.bundles"
+
+echo -n "Updating previous symlinks"
+
+for file in $FILES; do
+  echo -n "."
+  rm ~/.$file                # Remove old file
+  ln -s $DIR/$file ~/.$file # Add new reference.
+done
+
+echo "Done!"
